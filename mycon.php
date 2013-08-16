ef64d1b97663982fd503085e98cfef8f0b777fe6
James Campbell <james@jamescampbell.us>
2013-08-16 02:26:22 -0400

    added mysql connection and storing values in database

--- mycon.php
+++ mycon.php
@@ -1,10 +1,13 @@
 <?php
 // Create connection
-$con=mysqli_connect("internal-db.s135538.gridserver.com","db135538_robot","!1V3r1tas123","db135538_btrnt");
-
+$con=mysql_connect("internal-db.s135538.gridserver.com","db135538_robot","!1V3r1tas123","db135538_btrnt");
+$db_selected = mysql_select_db('db135538_btrnt', $con);
 // Check connection
-if (mysqli_connect_errno($con))
+if (!$con)
   {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  echo "Failed to connect to MySQL: " . mysql_connect_error();
   }
+  else { $evidence = 'yes';//echo "<div style='display:block'>Connection success mofo!</div>";
+  }
+
 ?>
