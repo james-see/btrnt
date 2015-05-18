1ea0266ce8aafc784e50685a750842dfd4d5dbfa
James Campbell <james@jamescampbell.us>
2015-05-17 21:31:44 -0400

    Update mycon.php

--- mycon.php
+++ mycon.php
@@ -1,6 +1,6 @@
 <?php
 // Create connection
-$con=mysql_connect("internal-db.s135538.gridserver.com","db135538_robot","!1V3r1tas123","db135538_btrnt");
+$con=mysql_connect("internal-db.s135538.gridserver.com","db135538_robot","password","db135538_btrnt");
 $db_selected = mysql_select_db('db135538_btrnt', $con);
 // Check connection
 if (!$con)
